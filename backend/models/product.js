56a3d1312af26acf64ae16393544e0a2860d5568
Fisnik <fisnik-canaj-test@gmail.com>
2021-08-28 20:39:09 +0200

    missed brand field added

--- backend/models/product.js
+++ backend/models/product.js
@@ -5,6 +5,10 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    brand: {
+        type: String,
+        required: true,
+    },
     description: {
         type: String,
         required: true
--- backend/routers/products.js
+++ backend/routers/products.js
@@ -62,6 +62,7 @@ router.post(`/`, uploadOptions.single('image'), async (req, res) => {
     if(!file) return res.status(400).send('No image in the request!');
     const fileName = req.file.filename;
     const basePath = `${req.protocol}://${req.get('host')}/public/upload/`
+    console.log(req.body);
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
